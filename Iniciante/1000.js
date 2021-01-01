3bbbfeeccb60d62c38a04dd2c9ecbda940884be2
Raphael Monterrazo <monterrazo.raphael@gmail.com>
2021-01-01 12:31:29 -0300

    Resolução do Problema 1000 - URI Judge

---
+++ Iniciante/1000.js
@@ -0,0 +1,13 @@
+//SUBMISSÃO: 21001878
+//Problema:  1000 - Hello World! 
+//Resposta:  Accepted 
+//Linguagem: JavaScript (nodejs 8.4.0) [+2s] {beta} 
+//Tempo:     0.452s
+//Tamanho:   28 Bytes 
+//Submissão: 01/01/2021 12:28:10 
+
+console.log('Hello World!');
+
+
+
+
